b580845f7386197ca9356b17a7eadbada71e0c09
lewisbrotchie <lewis@yulife.com>
2019-09-13 17:07:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const config = require("./utils/config");
+const app = require("./app");
+
+require("dotenv").config();
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
